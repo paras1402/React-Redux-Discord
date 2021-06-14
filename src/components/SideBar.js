03c82504eea8bbf50cde3358f786046fabd54215
paras <paraskangra14@gmail.com>
2021-06-14 22:21:55 +0530

    started

---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SideBar.css";
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <h1>I am SideBar</h1>
+    </div>
+  );
+}
+
+export default SideBar;
